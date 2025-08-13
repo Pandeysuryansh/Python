e7dcd02055b775d470475b71965fe4d7bf63d09b
Pandeysuryansh <167824825+Pandeysuryansh@users.noreply.github.com>
2025-08-13 17:27:58 +0530

    Add files via upload

---
+++ python4.py
@@ -0,0 +1,53 @@
+salary = 2E4
+print(salary)
+print(type(salary))
+
+
+#complex
+print(2+3j)
+print(type(2+3j))
+
+
+#string
+print(type("hello"))
+print(type('hello'))
+print(type("""hello"""))
+print(type('''hello'''))
+
+name ="himanshu"
+print(len(name))
+
+print(name[4])
+
+
+#list
+mylist = ["USA",21,[1,2,3],2+4j,20,3]
+print(mylist)
+print(type(mylist))
+
+print(mylist[3])
+mylist[2] = "23"
+print(mylist)
+
+#tuple
+t = ("USA",22,20.9,[1,2,3])
+print(t)
+# we cannot change tuple elements 
+
+
+#dictionary
+d= {"rohan":1200,"sachin":2100,"himanshu":2300}
+print(type(d))
+print(d["sachin"])
+
+#set
+nums = {1,1,1,2,22,3,4,4,4}
+print(nums)
+print(type(nums))
+
+#range 
+print(list(range(0,9,2)))
+
+#MCQ
+
+print(11,12,13,14,15)
